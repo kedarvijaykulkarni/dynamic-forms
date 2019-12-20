0971ed227a874bd76089c4552165585c63189aa5
Kedar Kulkarni <kedar@safechin.io>
2019-12-21 02:25:35 +0530

    added form element dynamically

--- app/routes/dynamic-forms.js
+++ app/routes/dynamic-forms.js
@@ -1,4 +1,63 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return [
+      {
+        "schema": [
+          {
+            "title": "First Name",
+            "type": "string",
+            "id": "firstName",
+            "name": "first-name",
+            "maxlength": 5,
+            "required": true,
+            "attributes": {
+              "data-test": "test-first-name",
+              "class": "input-element"
+            }
+          },
+          {
+            "title": "Last Name",
+            "type": "string",
+            "id": "lastName",
+            "name": "last-name",
+            "maxlength": 50,
+            "required": true,
+            "attributes": {
+              "data-test": "test-last-name",
+              "class": "input-element"
+            }
+          },
+          {
+            "title": "Email",
+            "type": "email",
+            "id": "email",
+            "name": "email",
+            "required": true,
+            "attributes": {
+              "data-test": "test-email",
+              "class": "input-element"
+            }
+          },
+          {
+            "title": "Birthday",
+            "type": "date",
+            "id": "birthDate",
+            "name": "birth-date",
+            "attributes": {
+              "data-test": "test-email",
+              "class": "input-element"
+            }
+          }
+        ]
+      },
+      {
+        "actions": {
+          "submit": "onSubmitAction"
+        }
+      }
+    ]
+  }
+
 });
